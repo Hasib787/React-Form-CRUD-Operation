5c25f3669e50f14ec57ffa07984b0644c7ca690e
Md. Masud Rana <mr.hasib45@gmail.com>
2021-11-07 19:09:07 +0600

    added component

---
+++ src/component/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserProfileForm from '../UserProfile';
+
+const Home = () => {
+    return (
+        <div>
+            <UserProfileForm />
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/component/UserProfile/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserProfileForm = () => {
+    return (
+        <div>
+            <h2>This is form</h2>
+        </div>
+    );
+};
+
+export default UserProfileForm;
